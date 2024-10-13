c318518a6de277ff7c085ccbe2772932c2a3c38c
Abderrahman Jbari <ajbari@c2r7p3.1337.ma>
2024-10-13 14:38:39 +0100

    fixed builtcheck to check compared with the length command so the (p doesn't run pwd...) and still fixing echo to print the fl ags correctly

--- builtins/builtin_check.c
+++ builtins/builtin_check.c
@@ -5,17 +5,17 @@ t_builtins_type    builtin_check(char *cmd)
     uint32_t length;
 
     length = ft_strlen(cmd);
-    if (!ft_strncmp(cmd, "cd", length))
+    if (!ft_strncmp(cmd, "cd", length) && ft_strlen(cmd) == 2)
         return (CD);
-    else if (!ft_strncmp(cmd, "export", length))
+    else if (!ft_strncmp(cmd, "export", length) && ft_strlen(cmd) == 6)
         return (EXPORT);
-    else if (!ft_strncmp(cmd, "env", length))
+    else if (!ft_strncmp(cmd, "env", length) && ft_strlen(cmd) == 3)
         return (ENV);
-    else if (!ft_strncmp(cmd, "exit", length))
+    else if (!ft_strncmp(cmd, "exit", length) && ft_strlen(cmd) == 4)
         return (EXIT);
-    else if (!ft_strncmp(cmd, "pwd", length))
+    else if (!ft_strncmp(cmd, "pwd", length) && ft_strlen(cmd) == 3)
         return (PWD);
-    else if (!ft_strncmp(cmd, "echo", length))
+    else if (!ft_strncmp(cmd, "echo", length) && ft_strlen(cmd) == 4)
         return (ECHO);
     return (NONE);
 }
--- builtins/mshell_echo.c
+++ builtins/mshell_echo.c
@@ -1,58 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mshell_echo.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajbari <ajbari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 13:27:39 by ajbari            #+#    #+#             */
+/*   Updated: 2024/10/13 14:33:21 by ajbari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/builtins.h"
 
-bool    is_flag(char *s)
+bool	is_flag(char *s, int *flag)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (s[i++] == '-')
+	i = 0;
+	if (s[i++] == '-')
+	{
+		while (s[i] && s[i] == 'n')
+			i++;
+		if (s[i] == '\0' && i > 1)
+			return (true);
+		else
+        {
+            if (*flag == -1)
+                *flag = 0;
+			return (false);
+        }
+	}
+	else
     {
-        while (s[i] == 'n')
-            i++;
-        if (s[i] == '\0')
-            return (true);
-        else 
-            return (false);
+        if (*flag == -1)
+            *flag = 0;
+		return (false);
     }
-    else
-        return (false);
 }
 
-int dp_size(char **args)
+int	dp_size(char **args)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    while (args[len])
-        len++;
-    return (len);
+	len = 0;
+	while (args[len])
+		len++;
+	return (len);
 }
-
-bool    mshell_echo(char **args)
+void    put_str(char *arg, int *space)
 {
-    int i;
-    int flag;
-    int args_end;
+    if (*space != -1)
+        ft_putstr_fd(" ", 1);
+    ft_putstr_fd(arg, 1);
+    *space = 1;
+    
+}
+bool	mshell_echo(char **args)
+{
+	int	i;
+	int	flag;
+	int	args_end;
+    int space;
 
-    args_end = dp_size(args);
-    flag = 0;
-    i = 1;
-    while (args[i])
-    {
-        if (is_flag(args[i]))
-        {
-            flag = is_flag(args[i]);
-            i++;
-            continue ;
-        }
-        else
-        {
-            ft_putstr_fd(args[i], 1);
-            i++;
-            if (!flag && i == args_end)
-                ft_putstr_fd("\n", 1);
-            continue ;
-        }
-    }
-    return (true);
+	args_end = dp_size(args);
+	flag = -1;
+	i = 1;
+    space = -1;
+	while (args[i])
+	{
+		if (flag != 0 && is_flag(args[i], &flag))
+		{
+			flag = is_flag(args[i], &flag);
+			i++;
+			continue ;
+		}
+		else
+		{
+            put_str(args[i], &space);
+			i++;
+			if ((!flag || flag == -1) && i == args_end)
+				ft_putstr_fd("\n", 1);
+			continue ;
+		}
+	}
+	return (true);
 }
