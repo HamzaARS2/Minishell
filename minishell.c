700f1cb4c2fc51641770e6582648f1f95d05333e
HamzaARS2 <hamza1arrastec@gmail.com>
2024-10-17 20:24:53 +0100

    norminette complete

--- minishell.c
+++ minishell.c
@@ -1,72 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: helarras <helarras@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 20:23:28 by helarras          #+#    #+#             */
+/*   Updated: 2024/10/17 20:23:29 by helarras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "include/minishell.h"
 #include <signal.h>
 
-int g_signal;
+int		g_signal;
 
-void    on_new_line(t_mshell *mshell, char *line)
+void	on_new_line(t_mshell *mshell, char *line)
 {
-    if (!mshell_parse(mshell, line))
-    {
-        free(mshell->lexer->content);
-        free_token_list(mshell->lexer->tokens);
-        free(mshell->lexer);
-        free(mshell->handler);
-        return ;
-    }
-    mshell_execute(mshell);
-    mshell_clean(mshell);
+	if (!mshell_parse(mshell, line))
+	{
+		free(mshell->lexer->content);
+		free_token_list(mshell->lexer->tokens);
+		free(mshell->lexer);
+		free(mshell->handler);
+		return ;
+	}
+	mshell_execute(mshell);
+	mshell_clean(mshell);
 }
 
-void sig_handler(int sig)
+void	sig_handler(int sig)
 {
-    if (sig == SIGINT)
-    {
-        g_signal = 1;
-        write(1, "\n", 1);
-        rl_on_new_line();
-        rl_replace_line("", 0);
-        rl_redisplay();
-     }
-    else if (sig == SIGQUIT)
-    {
-        ft_putstr_fd("Quit: 3\n", 2);
-        g_signal = 131;
-    }
+	if (sig == SIGINT)
+	{
+		g_signal = 1;
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	else if (sig == SIGQUIT)
+	{
+		ft_putstr_fd("Quit: 3\n", 2);
+		g_signal = 131;
+	}
 }
 
-void    display_prompt(t_mshell *mshell)
+void	display_prompt(t_mshell *mshell)
 {
-    char    *line;
-    
-    while (true)
-    {
-        signal(SIGINT, sig_handler);
-        signal(SIGQUIT, SIG_IGN);
-        line = readline("minishell > ");
-        if (g_signal)
-        {
-        	mshell->ex_status = g_signal;
-            g_signal = 0;
-        }
-        if (line)
-        {
-            add_history(line);
-            on_new_line(mshell, line);
-        }
-        else
-            break;
-    }
+	char	*line;
+
+	while (true)
+	{
+		signal(SIGINT, sig_handler);
+		signal(SIGQUIT, SIG_IGN);
+		line = readline("minishell > ");
+		if (g_signal)
+		{
+			mshell->ex_status = g_signal;
+			g_signal = 0;
+		}
+		if (line)
+		{
+			add_history(line);
+			on_new_line(mshell, line);
+		}
+		else
+			break ;
+	}
 }
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
-    t_mshell mshell;
-    (void)ac;
-    (void)av;
-    
-    
-    init_mshell(&mshell, env);
-    display_prompt(&mshell);
+	t_mshell	mshell;
 
-    return (mshell.ex_status);
+	(void)ac;
+	(void)av;
+	init_mshell(&mshell, env);
+	display_prompt(&mshell);
+	return (mshell.ex_status);
 }
--- shell_control.c
+++ shell_control.c
@@ -6,14 +6,14 @@
 /*   By: helarras <helarras@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:49:02 by helarras          #+#    #+#             */
-/*   Updated: 2024/10/17 19:54:33 by helarras         ###   ########.fr       */
+/*   Updated: 2024/10/17 20:24:23 by helarras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/execution.h"
 #include "include/minishell.h"
 
-extern int g_signal;
+extern int	g_signal;
 
 void	init_mshell(t_mshell *mshell, char **env)
 {
@@ -48,39 +48,27 @@ bool	mshell_parse(t_mshell *mshell, char *line)
 
 void	mshell_execute(t_mshell *mshell)
 {
-    init_executor(&mshell->executor, &mshell->envlst, &mshell->ex_status);
-    // TODO: execute the whole AST.
-    // TODO: execute the whole AST.
-    if (!hrdoc_collect(mshell->parser->ast, mshell->envlst, &mshell->ex_status))
+	init_executor(&mshell->executor, &mshell->envlst, &mshell->ex_status);
+	if (!hrdoc_collect(mshell->parser->ast, mshell->envlst, &mshell->ex_status))
 	{
 		mshell->ex_status = 1;
 		return ;
 	}
-    if (mshell->ex_status == -2)
-    {
-        mshell_clean(mshell);
-        exit(2);
-    }
-    exec(mshell->parser->ast, &(mshell->executor));
-    //TODO: FREE THE PATHS;
-    ft_wait(&mshell->executor);
-
-    //* HAMZA'S PREVIOUS PROTOTYPE
-    /*     t_executor executor;
-
-        init_executor(&executor, mshell->parser->ast, NULL);
-        execute_ast(&executor); */
+	if (mshell->ex_status == -2)
+	{
+		mshell_clean(mshell);
+		exit(2);
+	}
+	exec(mshell->parser->ast, &(mshell->executor));
+	ft_wait(&mshell->executor);
 }
 
 void	mshell_clean(t_mshell *mshell)
 {
-	// lexer cleaner
 	free(mshell->lexer->content);
-	// resolver cleaner
 	rslv_clean(mshell->resolver);
 	prsr_clean(mshell->parser);
 	exec_clean(&mshell->executor);
-	// ast cleaner
 	free(mshell->lexer);
 	free(mshell->handler);
 	free(mshell->resolver);
